603e13778d47efc8245c003a5de54144774a0acb
7x <info@se7enx.com>
2026-08-07 21:37:14 -0700

    Updated: bump xrowmetadata version to 1.3.7

--- ezinfo.php
+++ ezinfo.php
@@ -11,7 +11,7 @@ class xrowmetadataInfo
     {
         return array(
             'Name' => "xrowmetadata",
-            'Version' => "1.3.6",
+            'Version' => "1.3.7",
             'Copyright' => "Copyright (C) 1998 - 2026 7x and 1999 - 2022 xrow GmbH",
             'Author' => "xrow GmbH",
             'License' => "GNU General Public License v2.0" );
